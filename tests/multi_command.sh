d893adb407cd684155f9b09c07233da73d271268
jaystu <johnstu32@gmail.com>
2016-03-05 19:02:46 -0800

    finished scripts

--- tests/multi_command.sh
+++ tests/multi_command.sh
@@ -3,5 +3,3 @@ echo "testing echo 1 && echo 2. Expected output \"1\" and \"2\""
 echo 1 && echo 2
 echo "testing echo 1 ; echo 2 && echo 3 || echo 4 && echo 5. Expected output: \"1\",\"2\",and \"3\""
 echo 1 ; echo 2 && echo 3
-echo "testing false && echo 1 || echo 2. Expected output \"2\""
-false && echo 1 || echo 2
--- tests/parenthesis_command.sh
+++ tests/parenthesis_command.sh
@@ -5,4 +5,11 @@ echo "testing (echo K; echo O) && (echo P || echo T). Expected output: \"K\" and
 (echo K; echo O) && (echo P || echo T)
 echo "testing (echo A || echo B) && (echo C || echo D). Expected output: \"A\" and \"C\""
 (echo A || echo B) && (echo C || echo D)
-
+echo "testing (false || echo 1 && echo 3) && (echo 6 || echo 7). Expected output \"1\" and \"6\""
+(false || echo 1 && echo 3) && (echo 6 || echo 7)
+echo "make sure in root directory. testing ([makefile] && echo 1) || echo 2. Expected output \"(true)\" and \"1\"
+([makefile] && echo 1) || echo 2
+echo "make sure in root directory. testing ((test -d src || echo 1) && echo 2) && echo 3. Expected output \"(true)\" and \"2\" and \"3\"
+((test -d src || echo 1) && echo 2) && echo 3
+echo "make sure in root directory. testing ([-e asdfasdf] || echo 1) && echo 2 || echo 3. Expected output \"(false)\" and \"1\" and \"2\"
+([-e asdfasdf] || echo 1) && echo 2 && echo 3
